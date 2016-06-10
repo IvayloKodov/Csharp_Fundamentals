3b3903587742d1a429e9bf56d6c3850445710663
IvayloKodov <Ivailo_Kodov@abv.bg>
2016-06-10 12:43:57 +0300

    .

---
+++ LINQ/LittleJohn.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+public class LittleJohn
+{
+    public static void Main()
+    {
+        string pattern = @"([>]{3}-----[>]{2})|([>]{2}-----[>]{1})|([>]{1}-----[>]{1})";
+        Regex regex = new Regex(pattern);
+        string input = Console.ReadLine();
+        // large medium small
+        Dictionary<string, int> counter = new Dictionary<string, int>();
+        counter.Add("large", 0);
+        counter.Add("medium", 0);
+        counter.Add("small", 0);
+
+        for (int i = 0; i < 4; i++)
+        {
+            MatchCollection match = regex.Matches(input);
+            if (match.Count > 0)
+            {
+                foreach (Match arrow in match)
+                {
+                    if (arrow.Value == ">>>----->>")//large
+                    {
+                        counter["large"]++;
+                    }
+                    else if (arrow.Value == ">>----->")//medium
+                    {
+                        counter["medium"]++;
+                    }
+                    else
+                    {
+                        counter["small"]++;
+                    }
+                }
+            }
+            input = Console.ReadLine();
+        }
+        string result = counter["small"].ToString() + counter["medium"] + counter["large"];
+        int resultInDec = int.Parse(result);
+        result = Convert.ToString(resultInDec, 2);
+
+        StringBuilder finalResult = new StringBuilder(result);
+        for (int i = result.Length - 1; i >= 0; i--)
+        {
+            finalResult.Append(result[i]);
+        }
+        double finalResultInDecimal = 0;
+        Console.WriteLine(Convert.ToInt32(finalResult.ToString(), 2));
+    }
+}
---
+++ LINQ/OfficeStuff.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+public class OfficeStuff
+{
+    public static void Main()
+    {
+        string pattern = @"^\|([A-Za-z]+) - (\d+) - ([A-Za-z]+)\|$";
+        Regex regex = new Regex(pattern);
+        var officeStuff = new SortedDictionary<string, Dictionary<string, int>>();
+
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            string input = Console.ReadLine().Trim();
+            Match match = regex.Match(input);
+            if (match.Success)
+            {
+                string company = match.Groups[1].Value;
+                int quantity = int.Parse(match.Groups[2].Value);
+                string product = match.Groups[3].Value;
+
+                if (!officeStuff.ContainsKey(company))
+                {
+                    officeStuff.Add(company, new Dictionary<string, int>());
+                }
+                if (!officeStuff[company].ContainsKey(product))
+                {
+                    officeStuff[company].Add(product, 0);
+                }
+                officeStuff[company][product] += quantity;
+            }
+        }
+        foreach (var company in officeStuff)
+        {
+            Console.Write($"{company.Key}: ");
+            int counter = 0;
+            foreach (var product in company.Value)
+            {
+                if (counter == company.Value.Count - 1)
+                {
+                    Console.Write($"{product.Key}-{product.Value}");
+                }
+                else
+                {
+                    Console.Write($"{product.Key}-{product.Value}, ");
+                }
+                counter++;
+            }
+            Console.WriteLine();
+        }
+    }
+}
